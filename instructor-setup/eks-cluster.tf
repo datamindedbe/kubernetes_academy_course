5440a8cc19b6b5cb341485cc89a9a155f999de2d
Niels Claeys <niels.claeys@gmail.com>
2022-08-24 20:16:05 +0200

    correct cluster

--- instructor-setup/eks-cluster.tf
+++ instructor-setup/eks-cluster.tf
@@ -22,6 +22,36 @@ module "eks" {
     disk_size      = 50
     instance_types = ["t3.large"]
   }
+  
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
 
   eks_managed_node_groups = {
     default_node_group = {
